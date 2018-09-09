67c97851d88dc3701ec89240e762511af9c64e02
Javier Carrillo Milla <stringparser@gmail.com>
2018-09-10 00:00:42 +0200

    chore(fonts): add missing font weights, rebuild

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -1,9 +1,12 @@
 export const GOOGLE_FONTS_API = 'https://www.googleapis.com/webfonts/v1/webfonts';
 export const FONT_WEIGHT_NAMES = {
+  100: 'thin',
+  200: 'extra-light',
   300: 'light',
   400: 'regular',
   500: 'medium',
   600: 'semi-bold',
   700: 'bold',
+  800: 'extra-bold',
   900: 'black',
 };
